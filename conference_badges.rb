15a707da2ce418c6d365bf6ad5f82d2a710aeb1f
Lynden Lim <lyndenlim15@gmail.com>
2022-02-19 12:00:59 -0500

    commit tree 4a1f54a628d70ac33c823a5b5ee1e9025818034d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  badge_messages = []
+  name_array.each { |name| badge_messages.push("Hello, my name is #{name}.") }
+  badge_messages
+end
+
+def assign_rooms(speakers_array)
+  new_array = []
+  speakers_array.each_with_index { |speaker, index| new_array.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!") }
+  new_array
+end
+
+def printer(array_of_names)
+    batch_badge_creator(array_of_names).each {|person| puts person}  
+    assign_rooms(array_of_names).each {|person| puts person}
+end
+ printer(['a','b'])
